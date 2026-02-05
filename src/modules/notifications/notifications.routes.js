0a45d3b4cc7c8f2ab1043c14eddb7c1692bf09ad
mzokirovic <mzokirovic2@gmail.com>
2026-02-05 15:35:07 +0500

    yangilandi

--- src/modules/notifications/notifications.routes.js
+++ src/modules/notifications/notifications.routes.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const controller = require('./notifications.controller');
 
 router.get('/', controller.list);
-router.post('/:id/read', controller.markRead);
 router.post('/read-all', controller.markAllRead);
 router.post("/token", controller.registerToken);
 router.post('/test-push', controller.testPush);
